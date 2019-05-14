a3bf6709b1b148121239abfd942b8db7c5be178b
shadowfox3735 <normanpan180@gmail.com>
2019-05-14 15:04:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
         resp.write item.price
       end
     else
-      resp.write "Item not found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
