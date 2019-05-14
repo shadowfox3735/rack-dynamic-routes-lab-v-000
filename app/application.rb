d167596d79947ba3fc1164291affaf3c1f93d1a9
shadowfox3735 <normanpan180@gmail.com>
2019-05-14 15:00:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item = @@items.find?{|i| i.name == item_name}
       
       if item.nil?
-        "Item not found"
+        resp.write "Item not found"
         resp.status = 400
       else
         resp.write item.price
