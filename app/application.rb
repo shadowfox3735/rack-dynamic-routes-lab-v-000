b8203ea48c3774e1db4f524a94358a7797f9a211
shadowfox3735 <normanpan180@gmail.com>
2019-05-14 14:57:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     
-    if resp.path.match(/items/)
+    if req.path.match(/items/)
       item_name = req.path.split(/items/).last
       item = @@items.find{|i| i.name == song_name}
       
