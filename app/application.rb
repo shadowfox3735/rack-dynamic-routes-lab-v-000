255b11e01af82f57097626f54f05be1a9aa3a000
shadowfox3735 <normanpan180@gmail.com>
2019-05-14 14:59:03 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
     
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      item = @@items.find?{|i| i.name == song_name}
+      item = @@items.find?{|i| i.name == item.name}
       
       if item.nil?
         "Item not found"
